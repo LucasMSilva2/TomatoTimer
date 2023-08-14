11deaea39e7ad9a0f67fde81c6bba032b8748326
lucas <lucastorrao691@gmail.com>
2023-08-13 21:41:57 -0300

    definição de fontes e cores

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { Button } from "./components/Button";
 import { defaultTheme } from './styles/themes/default';
-import { darkTheme } from './styles/themes/dark';
-import { useState } from 'react';
+//import { darkTheme } from './styles/themes/dark';
+//import { useState } from 'react';
 import { GlobalStyle } from './styles/global'
 
 export function App() {
-  const [theme, setTheme] = useState(defaultTheme);
-
-  const toggleTheme = () => {
-    setTheme((prevTheme)=> prevTheme === defaultTheme ? darkTheme : defaultTheme);
-  }
   
   return (
-    <ThemeProvider theme={theme}>
-      <Button onClick={toggleTheme} variant="primary"/>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
       <Button variant="secondary"/>
       <Button variant="success"/>
       <Button variant="danger"/>
